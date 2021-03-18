ebc655c0ddbd12b43e96db282ffc9d23df48ef36
maryam alnajjar <mariam.mohannad98@gmail.com>
2021-03-18 15:02:48 +0200

    edit

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -3,4 +3,4 @@ if(favouritestyle=='classic'){document.write('<img src="pic/classicf.jpg"/>');}e
 var age=prompt("How old are you?");
 if(age>=16){alert('Enjoy in our world ');}else{alert('Go to this and enjoy "https://www.cartoonnetworkhq.com/games"');}
 var im=prompt(' did you enjoy in our website?');
-if(im=='yes'){document.write('Nice to hear it');}else{document.write('Ooooh we are sorry!');}
+if(im=='yes'){alert('Nice to hear it');}else{alert('Ooooh we are sorry!');}
