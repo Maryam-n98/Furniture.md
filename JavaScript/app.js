52560b7020e3d9e3c1f7a4b97371da0ccfeb9907
maryam alnajjar <mariam.mohannad98@gmail.com>
2021-03-21 14:48:01 +0200

    edit

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,6 +1,41 @@
 var favouritestyle= prompt("what do you love classic or unique furniture?");
-if(favouritestyle=='classic'){document.write('<img src="pic/classicf.jpg"/>');}else if(favouritestyle=='unique'){document.write('<img src="pic/unique.jpg"/>');}
+
+if(favouritestyle=='classic'){
+    document.write('<img src="pic/classicf.jpg"/>');
+}else if(favouritestyle=='unique'){
+    document.write('<img src="pic/unique.jpg"/>');
+}
+
 var age=prompt("How old are you?");
-if(age>=16){alert('Enjoy in our world ');}else{alert('Go to this and enjoy "https://www.cartoonnetworkhq.com/games"');}
-var im=prompt(' did you enjoy in our website?');
-if(im=='yes'){alert('Nice to hear it');}else{alert('Ooooh we are sorry!');}
+
+if(age>=16){
+    alert('Enjoy in our world ');
+}else{
+    alert('Go to this and enjoy "https://www.cartoonnetworkhq.com/games"');
+}
+
+var enjoy=prompt(' did you enjoy in our website?');
+
+if(enjoy =='yes') {
+alert('Nice to hear it');
+}else
+{alert('Ooooh we are sorry!');
+}
+
+while (favouritestyle !== 'classic' && favouritestyle !== 'unique')
+ {favouritestyle = prompt('please only write classic or unique');
+}
+
+// if(stars<=5) {star +='<img src="pic/star.png"/>';}
+
+var stars= prompt('What star rating would you give us from 5');
+var result='';
+var star='<img src="pic/star.png"/>';
+
+var number=1;
+
+ for (var i= 0; i< stars; i++){
+     document.write(star + number);
+     number++
+
+ }
